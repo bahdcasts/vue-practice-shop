a5a8df1ba086de037ae4d0b8edc8dab6e00c5945
Kati Frantz <frantzenterprises777@gmail.com>
2018-07-12 07:22:56 +0100

    display cart items

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,23 @@
           </div>
         </div>
       </div>
-      <div class="col-md-5"></div>
+      <div class="col-md-5 my-5">
+        <cart :items="cart"></cart>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import products from '@/products.json'
+import Cart from '@/components/Cart.vue'
 import Product from '@/components/Product.vue'
 
 export default {
   name: 'app',
   components: {
-    Product
+    Product,
+    Cart
   },
   data() {
     return {
---
+++ src/components/Cart.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul class="list-group">
+    <li class="list-group-item" v-for="item in items" :key="item.id">
+      {{ item.title }} - $ {{ item.price }}
+      <button class="btn badge badge-danger float-right">Remove from cart</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['items']
+}
+</script>
+
